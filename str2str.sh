0fade9555657a2006d42e2f8bfbeb266efb2f73a
John ORaw <john.oraw@iotech.ie>
2022-02-12 20:01:47 +0000

    First Commit

---
+++ str2str.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# by: JOR
+# Date: 12FEB22
+# Function: Gets NTIP from remote and forwards to local serial port, also acts as an RTCM TCP server
+# Leave this script in /home/pi
+# Script: str2str.sh
+
+HOMEPATH="/home/pi"
+
+# Configure hardware serial port
+stty -F /dev/ttyAMA1 clocal raw speed 115200
+
+# Get RTCM3 from RTK2GO and send to Ardusimple
+$HOMEPATH/rtklib/str2str/str2str -in ntripcli://:@rtk2go.com/Umricam -out serial://ttyAMA1:115200:8:n:1 -out tcpsvr://:12345
